f74af08b35067f30e46e661c2d3d3dc8668b5983
andreice <aleefve@gmail.com>
2024-05-20 20:06:47 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -1,7 +1,25 @@
 package online.tekwillacademy;
 
+import online.tekwillacademy.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DriverManager manager = DriverManager.getInstance();
+        WebDriver driver = manager.getDriver();
+
+        String currentWindowName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://tekwillacademy-opencart.online/");
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        manager.getDriver().get("https://tekwillacademy-opencart.online/index.php?route=product/product&language=en-gb&product_id=42");
+        driver.quit();
+
+        System.out.println("The driver is closed!");
     }
 }
